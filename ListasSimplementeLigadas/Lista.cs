45c1b71e7d6c3919ae4f9a7abf82bf7491181b8b
Mayra Tovar <mayra_ale99@hotmail.com>
2025-02-15 10:50:35 -0700

    Agregamos clase listas

---
+++ ListasSimplementeLigadas/Lista.cs
@@ -0,0 +1,27 @@
+﻿namespace ListasSimplementeLigadas
+{
+    internal class Lista
+    {
+        private Nodo _nodoInicial;
+
+        public Lista() 
+        {
+            _nodoInicial = new Nodo();
+        }
+
+        public void Agregar(string dato)
+        {
+            Nodo nodoActual = _nodoInicial;
+
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+            }
+
+            Nodo nodoNuevo = new Nodo();
+            nodoNuevo.Dato = dato;
+
+            nodoActual.Siguiente = nodoNuevo;
+        }
+    }
+}
